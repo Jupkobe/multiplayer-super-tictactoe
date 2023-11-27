075dd5d2e0d71ba548290b14b8fd712583ac6ba1
Eyüp <84783072+Jupkobe@users.noreply.github.com>
2023-11-27 23:21:18 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,18 +11,24 @@ const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
 
-const corsOptions = {
-  origin: 'https://super-tictactoe-frontend.onrender.com/',
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-};
-
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://super-tictactoe-frontend.onrender.com');
-  res.header('Access-Control-Allow-Methods', 'GET, POST');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://super-tictactoe-frontend.onrender.com/"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
   next();
 });
 
