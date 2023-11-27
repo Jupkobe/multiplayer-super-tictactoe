ecb59a6cd1549fa692642af58ae616936a48ecaa
Eyüp <84783072+Jupkobe@users.noreply.github.com>
2023-11-27 23:10:10 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,13 @@ const corsOptions = {
   credentials: true,
 };
 
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://super-tictactoe-frontend.onrender.com');
+  res.header('Access-Control-Allow-Methods', 'GET, POST');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 server.listen(PORT, () => {
     console.log("SERVER IS UP")
