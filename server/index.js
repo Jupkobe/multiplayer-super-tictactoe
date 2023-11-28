3c4bcd1aa84ed21dc57d2a8574e1e5a9b06cf369
Eyüp <84783072+Jupkobe@users.noreply.github.com>
2023-11-28 17:10:55 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,27 +19,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
-// app.use((req, res, next) => {
-//   res.setHeader(
-//     "Access-Control-Allow-Origin",
-//     "https://super-tictactoe-frontend.onrender.com"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-//   );
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-//   res.setHeader("Access-Control-Allow-Private-Network", true);
-//   //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-//   res.setHeader("Access-Control-Max-Age", 7200);
-
-//   next();
-// });
-
 server.listen(PORT, () => {
     console.log("SERVER IS UP")
 });
